122922d835fe6d341612593259f4706f8653bf16
OTRSU <100841662+OTRSU@users.noreply.github.com>
2022-03-09 21:08:46 +0800

    just nothing

--- StudentPower.cpp
+++ StudentPower.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* 写的很差 */
 
 int main(){
 	float high;
