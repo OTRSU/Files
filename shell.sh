f396a420f53c688c326bb1ec11d8f10eb75994d0
OTRSU <100841662+OTRSU@users.noreply.github.com>
2023-04-05 20:40:33 +0800

    Add files via upload

---
+++ shell.sh
@@ -0,0 +1,8 @@
+wget http://mirrors.ustc.edu.cn/ubuntu/pool/universe/p/pacman-package-manager/pacman-package-manager_6.0.1-4_amd64.deb
+wget http://mirrors.ustc.edu.cn/ubuntu/pool/universe/p/pacman-package-manager/makepkg_6.0.1-4_amd64.deb
+wget http://mirrors.ustc.edu.cn/ubuntu/pool/universe/p/pacman-package-manager/libalpm-dev_13.0.2-3_amd64.deb
+wget http://mirrors.ustc.edu.cn/ubuntu/pool/universe/p/pacman-package-manager/libalpm13_6.0.1-4_amd64.deb
+dpkg -i pacman-package-manager_6.0.1-4_amd64.deb
+dpkg -i makepkg_6.0.1-4_amd64.deb
+dpkg -i libalpm-dev_13.0.2-3_amd64.deb
+dpkg -i libalpm13_6.0.1-4_amd64.deb
