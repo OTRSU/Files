9f6606483bbc82de956e81a5a9166dc8fe82c18a
OTRSU <100841662+OTRSU@users.noreply.github.com>
2023-04-05 21:21:15 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -6,6 +6,7 @@ cp makepkg.conf /etc/makepkg.conf
 cp pacman.conf /etc/pacman.conf
 mkdir -p /etc/pacman.d
 cp mirrorlist /etc/pacman.d/mirrorlist
+cp mirrorlist /etc/mirrorlist
 dpkg -i pacman-package-manager_6.0.1-4_amd64.deb
 dpkg -i makepkg_6.0.1-4_amd64.deb
 dpkg -i libalpm-dev_13.0.2-3_amd64.deb
