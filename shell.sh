72013d940ac09c45b562f7f14c4fe83f204e4999
OTRSU <100841662+OTRSU@users.noreply.github.com>
2023-04-05 21:06:05 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -6,3 +6,5 @@ dpkg -i pacman-package-manager_6.0.1-4_amd64.deb
 dpkg -i makepkg_6.0.1-4_amd64.deb
 dpkg -i libalpm-dev_13.0.2-3_amd64.deb
 dpkg -i libalpm13_6.0.1-4_amd64.deb
+wget https://raw.githubusercontent.com/OTRSU/Files/main/makepkg.conf 
+mv makepkg.conf /etc/makepkg.conf
