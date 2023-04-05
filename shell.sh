fd8d5b3f2046dabdc35870a5d2850e772dbc27a4
OTRSU <100841662+OTRSU@users.noreply.github.com>
2023-04-05 21:19:44 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -2,11 +2,12 @@ wget http://mirrors.ustc.edu.cn/ubuntu/pool/universe/p/pacman-package-manager/pa
 wget http://mirrors.ustc.edu.cn/ubuntu/pool/universe/p/pacman-package-manager/makepkg_6.0.1-4_amd64.deb
 wget http://mirrors.ustc.edu.cn/ubuntu/pool/universe/p/pacman-package-manager/libalpm-dev_13.0.2-3_amd64.deb
 wget http://mirrors.ustc.edu.cn/ubuntu/pool/universe/p/pacman-package-manager/libalpm13_6.0.1-4_amd64.deb
+cp makepkg.conf /etc/makepkg.conf 
+cp pacman.conf /etc/pacman.conf
+mkdir -p /etc/pacman.d
+cp mirrorlist /etc/pacman.d/mirrorlist
 dpkg -i pacman-package-manager_6.0.1-4_amd64.deb
 dpkg -i makepkg_6.0.1-4_amd64.deb
 dpkg -i libalpm-dev_13.0.2-3_amd64.deb
 dpkg -i libalpm13_6.0.1-4_amd64.deb
-mv makepkg.conf /etc/makepkg.conf 
-mv pacman.conf /etc/pacman.conf
-mv mirrorlist /etc/mirrorlist
 pacman -Sy
